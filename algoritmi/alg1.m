fbd4ea2450cf19e6278a5d3485ed601520518aa4
DejavuITA <Bazzanella.Davide@gmail.com>
2013-09-24 16:02:03 +0200

    modified alg1.m

--- algoritmi/alg1.m
+++ algoritmi/alg1.m
@@ -1,35 +1,19 @@
 function retval = mmhg_to_pa (v)
   retval = 0;
-  if (is_vector (v))
-    retval = v .* 133.322;
-  else
-    retval = v * 133.322;
-  endif
+  retval = v .* 133.322;
 endfunction
 
 function retval = pa_to_mmhg (v)
   retval = 0;
-  if (is_vector (v))
-    retval = v ./ 133.322;
-  else
-    retval = v / 133.322;
-  endif
+  retval = v ./ 133.322;
 endfunction
 
 function retval = atm_to_pa (v)
   retval = 0;
-  if (is_vector (v))
-    retval = v .* 101325;
-  else
-    retval = v * 101325;
-  endif
+  retval = v .* 101325;
 endfunction
 
 function retval = pa_to_atm (v)
   retval = 0;
-  if (is_vector (v))
-    retval = v ./ 101325;
-  else
-    retval = v / 101325;
-  endif
+  retval = v ./ 101325;
 endfunction
