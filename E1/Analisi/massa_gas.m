4bcca2f2abf168d3291aff88d158d79ea7dc96d7
Francesco Pasa <francescopasa@gmail.com>
2013-10-11 15:17:33 +0200

    quasi finita

--- E1/Analisi/massa_gas.m
+++ E1/Analisi/massa_gas.m
@@ -8,6 +8,8 @@ dati = csvread("../Dati/pesi.csv");
 volume = 0.002769;
 s_volume = 0.000005;
 
+moist = 0.58;
+
 T = 22 + 273;
 s_T = 1 / sqrt(12);
 
@@ -18,14 +20,26 @@ pieno = dati(2:end, 3)
 pressione = dati(2:end, 4)
 s_pressione = 10000 / sqrt(12);
 
+# correzione umidita
+pressione_H20 = moist * 1.6055e+11 * exp(-4.3883e04 / (8.3144 * T))
+pressione(1, 1) = pressione(1, 1) - pressione_H20
+
 massa = pieno .- vuoto
 s_massa = 0.0001 / sqrt(6);
+
+# correzione umidita
+moli_H2O = (pressione_H20 * volume) ./ (R .* T)
+massa_H20 = moli_H2O * 0.018
+massa(1, 1) = massa(1, 1) - massa_H20
 
 moli = (pressione .* volume) ./ (R .* T)
 s_moli = sqrt((volume ./ (R .* T) * s_pressione) .^ 2 .+
 	(pressione ./ (R .* T) * s_volume) .^ 2 .+
 	((pressione .* volume) ./ (R .* T .^ 2)) .^ 2)
-	
+
+# correzione umidita
+moli(1, 1) = moli(1, 1) - moli_H2O
+
 massa_mol = massa ./ moli;
 s_massa_mol = sqrt((s_massa ./ moli) .^ 2 .+
 	(massa ./ (moli .^ 2) .* s_moli) .^ 2);
