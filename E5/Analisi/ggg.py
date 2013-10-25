622704676bf16e6a3f8569e342a71168776954a4
Francesco Pasa <francescopasa@gmail.com>
2013-10-25 16:05:04 +0200

    frapa and gorilla rule

--- E5/Analisi/ggg.py
+++ E5/Analisi/ggg.py
@@ -1,11 +1,13 @@
 ## -*- encoding: utf-8 -*-
 
 import csv
+from math import *
 
 mpl = False
 try:
     import matplotlib
     from matplotlib import pyplot as plt
+    from scipy.misc import imread
     mpl = True
 except:
     print "installati matplotlib!"
@@ -24,50 +26,36 @@ with open('../Dati/rotativa-Pressione.csv') as csvfile:
 N = (1, 2, 3, 4, 5, 5.2, 5.4, 5.6, 5.8, 6, 7, 8, 9)
 
 Q = (
-   1.3980e-06,
-   1.3966e-06,
-   1.6286e-06,
-   3.0668e-06,
-   1.0383e-05,
-   1.3067e-05,
-   1.8431e-05,
-   2.6026e-05,
-   4.3632e-05,
-   9.6295e-05,
-   6.5170e-04,
-   4.1163e-03,
-   2.9884e-02
+    1.3980e-04,
+    1.3821e-04,
+    1.6286e-04,
+    3.0668e-04,
+    1.0383e-03,
+    1.3067e-03,
+    1.8431e-03,
+    2.6026e-03,
+    4.3632e-03,
+    9.6295e-03,
+    6.5170e-02,
+    4.1163e-01,
+    2.9884e+00
 )
 
 dQ = (
-   1.8617e-08,
-   1.8197e-08,
-   2.2002e-08,
-   2.7229e-08,
-   1.3016e-07,
-   8.3042e-08,
-   1.1322e-07,
-   1.6312e-07,
-   2.6838e-07,
-   1.2289e-06,
-   5.5883e-06,
-   5.6583e-05,
-   6.7334e-04
+      1.8617e-06,
+      1.9620e-06,
+      2.2002e-06,
+      2.7229e-06,
+      1.3016e-05,
+      8.3042e-06,
+      1.1322e-05,
+      1.6312e-05,
+      2.6838e-05,
+      1.2289e-04,
+      5.5883e-04,
+      5.6583e-03,
+      6.7334e-02
 )
-
-   1.3980e-04   1.8617e-06
-   1.3821e-04   1.9620e-06
-   1.6286e-04   2.2002e-06
-   3.0668e-04   2.7229e-06
-   1.0383e-03   1.3016e-05
-   1.3067e-03   8.3042e-06
-   1.8431e-03   1.1322e-05
-   2.6026e-03   1.6312e-05
-   4.3632e-03   2.6838e-05
-   9.6295e-03   1.2289e-04
-   6.5170e-02   5.5883e-04
-   4.1163e-01   5.6583e-03
-   2.9884e+00   6.7334e-02
 
 def limit(da, a):
     return sum(pressione[da:a]) / (a - da);
@@ -108,6 +96,9 @@ P_turbo = (
 )
 
 S = [Q[3 + n]/p for n, p in enumerate(P2)]
+dS = [sqrt((dq/p)**2 + (q*p*0.1/p**2)**2) for p, q, dq in zip(P2, Q[3:], dQ[3:])]
+S_turbo = [Q[n]/p for n, p in enumerate(P_turbo)]
+dS_turbo = [sqrt((dq/p)**2 + (q*p*0.1/p**2)**2) for p, q, dq in zip(P_turbo, Q[:10], dQ[:10])]
 
 print Q
 print
@@ -115,7 +106,9 @@ print "\n".join(map(str, P))
 print 
 print "\n".join(map(str, P2))
 print
-print "\n".join(map(str, [s for s in S]))
+print "\n".join([str(s*3600) + "   " + str(sd * 3600) for s, sd in zip(S, dS)])
+print
+print "\n".join([str(s*1000) + "   " + str(sd * 1000) for s, sd in zip(S_turbo, dS_turbo)])
 
 if mpl:
     f = plt.figure(figsize=(10, 7.5))
@@ -128,15 +121,64 @@ if mpl:
     ax.errorbar(x=tempo[1500:], y=pressione[1500:],
         fmt="o", c="black")
 
-    ax.set_ylabel(r'Pressine [Pa]',
+    ax.set_ylabel(r'Pressione [Pa]',
         labelpad=6, fontsize=14)
     ax.set_xlabel(u'Tempo [s]',
         labelpad=6, fontsize=14)
     
-    #ax.set_xlim((0, 10))
     ax.grid(True)
 
     f.subplots_adjust(left=0.10, right=0.95,
+        top=0.90, bottom=0.10, hspace=0.1)
+    
+
+    ### PLOT 2 ##################################################
+    f1 = plt.figure(figsize=(10, 7.5))
+    f1.suptitle(u"Velocità di pompaggio pompa rotativa",
+        y=0.96, fontsize=16)
+
+    ax1 = f1.add_subplot(1, 1, 1)
+    ax1.set_xscale('log')
+    #ax1.set_yscale('log')
+
+    ax1.errorbar(x=P2, y=[s*1000 for s in S], yerr=[ds*1000 for ds in dS],
+        fmt="o", c="black")
+
+    ax1.set_xlabel(u'Pressione [Pa]',
+        labelpad=6, fontsize=14)
+    ax1.set_ylabel(u'Velocità di pompaggio S [$\\times 10^{-3}\; m^3 s^{-1}$]',
+        labelpad=6, fontsize=14)
+    
+    ax1.set_ylim((0, 2))
+    #ax1.set_xticks(("0", "10", "100", "1000", "10000"))
+    ax1.grid(True)
+
+    f1.subplots_adjust(left=0.10, right=0.95,
+        top=0.90, bottom=0.10, hspace=0.1)
+
+    ### PLOT 2 ##################################################
+    f2 = plt.figure(figsize=(10, 7.5))
+    f2.suptitle(u"Velocità di pompaggio pompa turbomolecolare",
+        y=0.96, fontsize=16)
+
+    ax2 = f2.add_subplot(1, 1, 1)
+    ax2.set_xscale('log')
+    #ax2.set_yscale('log')
+
+    ax2.errorbar(x=P_turbo, y=[s*1000 for s in S_turbo], yerr=[ds*1000 for ds in dS_turbo],
+        fmt="o", c="black")
+
+    ax2.set_xlabel(u'Pressione [Pa]',
+        labelpad=6, fontsize=14)
+    ax2.set_ylabel(u'Velocità di pompaggio S [$\\times 10^{-3}\; m^3 s^{-1}$]',
+        labelpad=6, fontsize=14)
+
+    
+    #ax2.set_xlim((1e-3, 0.25));
+    #ax2.set_ylim((25, 50));
+    ax2.grid(True)
+
+    f2.subplots_adjust(left=0.10, right=0.95,
         top=0.90, bottom=0.10, hspace=0.1)
 
     plt.show()
