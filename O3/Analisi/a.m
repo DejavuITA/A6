20c2dfb209b10f768b1aca0b7103d9192777dfa0
Francesco Pasa <francescopasa@gmail.com>
2013-11-27 09:01:56 +0100

    ajfa

--- O3/Analisi/a.m
+++ O3/Analisi/a.m
@@ -11,10 +11,17 @@ dat_dat_pow = csvread("../Dati/dati.csv")(2:end, 1);
 dat_dat_gradi = csvread("../Dati/dati.csv")(2:end, 2);
 dat_dat_primi = csvread("../Dati/dati.csv")(2:end, 3);
 
+dangle = 0.25 .* pi ./ 180;
+
 Int_max = mean(dat_Int_max_pow)
-theta_max = mean(pi.*(dat_Int_max_gradi .+ dat_Int_max_primi./60)./180)
+dInt_max = sqrt(mean((dat_Int_max_pow .- Int_max) .^ 2) .+ 0.01.^2);
+theta_max = mean(pi.*(dat_Int_max_gradi .+ dat_Int_max_primi./60)./180);
+dtheta_max = sqrt(mean((pi.*(dat_Int_max_gradi .+ dat_Int_max_primi./60)./180 .- theta_max) .^ 2) .+ dangle.^2);
 Int = dat_dat_pow;
+dInt = 0.01 .* ones(length(Int), 1);
 theta = pi.*(dat_dat_gradi .+ dat_dat_primi./60)./180;
+dtheta = dangle .* ones(length(theta), 1);
+
 
 % altri dati
 
@@ -22,26 +29,34 @@ theta = pi.*(dat_dat_gradi .+ dat_dat_primi./60)./180;
 clear dat_Int_max_pow dat_Int_max_gradi dat_Int_max_primi dat_dat_pow dat_dat_gradi dat_dat_primi;
 
 % analisi dati
-Int_rel = Int ./ Int_max
+Int_rel = Int ./ Int_max;
+dInt_rel = sqrt((dInt ./ Int_max).^2 .+ (Int .* dInt_max ./ Int_max .^2).^2);
 %x = sin(theta);
 
-
+display("\n\nDATI PER GRAFICO ANDREA");
 %[Int_rel x]
 New_Int = [Int_rel' 1]'; % :-) cmq bastava mettere ; al posto di spazio senza trasporre nulla
+dNew_Int = [dInt_rel; dInt_max / Int_max];
 New_theta = [theta' theta_max]' .- theta_max;
+dNew_theta = [sqrt(dtheta.^2 .+ dtheta_max.^2); sqrt(2) .* dtheta_max];
 x = sin(New_theta);
-New_Int = [New_Int(45)' New_Int(1:25)' New_Int(26:44)']';
-x = [x(45)' x(1:25)' x(26:44)']';
-New_theta = [New_theta(45)' New_theta(1:25)' New_theta(26:44)']';
-[New_Int x]
- 
+dx = (cos(New_theta).*dNew_theta);
+New_Int = [New_Int(45); New_Int(1:25); New_Int(26:44)]
+dNew_Int = [dNew_Int(45); dNew_Int(1:25); dNew_Int(26:44)]
+x = [x(45); x(1:25); x(26:44)];
+dx = [dx(45); dx(1:25); dx(26:44)];
+New_theta = [New_theta(45); New_theta(1:25); New_theta(26:44)]
+New_theta = [dNew_theta(45); dNew_theta(1:25); dNew_theta(26:44)]
+
+errorbar(x, New_Int, dx, dNew_Int)
+
  % non so cosa faccio Andrea
-dy = 0.001
+dy = 0.001;
 % questi due sotto non c'entrano nulla
 %d_y_1 = sqrt((1./Int.^2)*(dx^2))
 %d_y_2 = sqrt((((theta./(Int.^2)).^2)*(dy^2)))
 % errore sulle y relative.
-dy_tot = sqrt((1/Int_max^2)*(dy^2).+(((Int./(Int_max^2)).^2)*(dy^2)))
+dy_tot = sqrt((1/Int_max^2)*(dy^2).+(((Int./(Int_max^2)).^2)*(dy^2)));
 
 % questo può tornare utile
 % inizio analisi% scrivo i dati
