76494603e9caaac60a6b76dbd5a68805c3b06d8b
Francesco Pasa <francescopasa@gmail.com>
2014-06-04 12:02:49 +0200

    str

--- chimica/C4/analisi/calc.py
+++ chimica/C4/analisi/calc.py
@@ -24,3 +24,15 @@ cf100 = c1000 * vi100 / vft
 cf10 = c1000 * vi10 / vft
 cf1 = cf100 * vi10 / vft
 print cf100, cf10, cf1
+
+ca_latte = ufloat(10.5, 10.5*0.04)
+latte = vft * ca_latte / vi10
+
+ca_succo = ufloat(10.6, 10.6*0.04)
+succo = ufloat(80, 1) * ca_latte / ufloat(20, 1)
+
+print latte, succo
+
+cs = unumpy.uarray([14.3, 33.4, 460, 14.8, 10.5, 10.6], np.array([14.3, 33.4, 460, 14.8, 10.5, 10.6])*0.04)
+f = cs / 4.0
+print f
